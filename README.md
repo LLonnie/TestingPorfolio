b0deaf2de98f4c29d49642c97c0cfbdfb3298844
Lonnie <llonnie21@gmail.com>
2026-02-04 06:50:29 -0700

    test: PW-1 setup Playwright so it will launch and run sample spec

---
+++ README.md
@@ -0,0 +1,3 @@
+## A collection of tests to showcase ability with various tools and frameworks.
+
+For more details on each tool see the README within that directory.
---
+++ playwright/playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
+  reporter: "html",
+  use: {
+    // baseURL: 'http://localhost:3000',
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+});
