c75e8996656220e92bf6f332264f87fb9ba189ca
Lonnie <llonnie21@gmail.com>
2026-02-04 07:27:41 -0700

    test(e2e): PW-2 add setup to playwright config

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -8,25 +8,34 @@ export default defineConfig({
   fullyParallel: true,
   reporter: "html",
   use: {
-    // baseURL: 'http://localhost:3000',
+    baseURL: "https://www.saucedemo.com/",
+    testIdAttribute: "data-test",
     trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
+      name: "setup",
+      testDir: "./.auth",
+      testMatch: /.*\.setup\.ts/,
+    },
+    {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
+      dependencies: ["setup"],
     },
 
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
+      dependencies: ["setup"],
     },
 
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
+      dependencies: ["setup"],
     },
   ],
 });
