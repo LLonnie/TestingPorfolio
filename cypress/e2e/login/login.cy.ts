7c6093cb73e23221a848ae790a6b2726f35a7991
Lonnie <llonnie21@gmail.com>
2026-02-08 07:29:15 -0700

    test(e2e): Cy-1 include basic login test

--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+it("Can login", () => {
+  cy.visit("/");
+
+  cy.findByTestId("username").type(Cypress.expose("STANDARD_USER"));
+  cy.findByTestId("password").type(Cypress.expose("PASSWORD"));
+  cy.findByTestId("login-button").click();
+
+  cy.url().should("contain", "/inventory.html");
+  cy.findByTestId("inventory-list").should("be.visible");
+});
