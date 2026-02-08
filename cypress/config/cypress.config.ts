e0f01237f35872125ad75d3dadf20e2aa08e2a60
Lonnie <llonnie21@gmail.com>
2026-02-08 08:23:30 -0700

    test(e2e): Cy-2 add process vars to cypress env

--- cypress/config/cypress.config.ts
+++ cypress/config/cypress.config.ts
@@ -1,23 +1,26 @@
 import { defineConfig } from "cypress";
 import path from "path";
+import * as dotenv from "dotenv";
 
-require("dotenv").config();
+dotenv.config({
+  path: path.resolve(__dirname, ".env"),
+  quiet: true,
+});
 
 export default defineConfig({
   allowCypressEnv: false,
-
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
+    env: {
+      standardUser: process.env.STANDARD_USER,
+      lockedUser: process.env.LOCKED_USER,
+      problemUser: process.env.PROBLEM_USER,
+      performanceUser: process.env.PERFORMANCE_USER,
+      errorUser: process.env.ERROR_USER,
+      visualUser: process.env.VISUAL_USER,
+      password: process.env.PASSWORD,
+    },
     supportFile: path.resolve(__dirname, "../support/e2e.ts"),
     specPattern: path.resolve(__dirname, "../e2e/**/*.cy.ts"),
-    setupNodeEvents(on, config) {
-      config.env.STANDARD_USER = process.env.STANDARD_USER;
-      config.env.LOCKED_USER = process.env.LOCKED_USER;
-      config.env.PROBLEM_USER = process.env.PROBLEM_USER;
-      config.env.PERFORMANCE_USER = process.env.PERFORMANCE_USER;
-      config.env.ERROR_USER = process.env.ERROR_USER;
-      config.env.VISUAL_USER = process.env.VISUAL_USER;
-      config.env.PASSWORD = process.env.PASSWORD;
-    },
   },
 });
