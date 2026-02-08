70d2355d7d2607f4e1f228bbc026ab8acbfc4afc
Lonnie <llonnie21@gmail.com>
2026-02-08 08:23:52 -0700

    test(e2e): Cy-2 update login specs to cover more flows

--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -1,10 +1,27 @@
 it("Can login", () => {
-  cy.visit("/");
+  cy.env(["standardUser", "password"]).then(({ standardUser, password }) => {
+    cy.visit("/");
 
-  cy.findByTestId("username").type(Cypress.expose("STANDARD_USER"));
-  cy.findByTestId("password").type(Cypress.expose("PASSWORD"));
-  cy.findByTestId("login-button").click();
+    cy.findByTestId("username").type(standardUser);
+    cy.findByTestId("password").type(password);
+    cy.findByTestId("login-button").click();
 
-  cy.url().should("contain", "/inventory.html");
-  cy.findByTestId("inventory-list").should("be.visible");
+    cy.url().should("contain", "/inventory.html");
+    cy.findByTestId("inventory-list").should("be.visible");
+  });
+});
+
+it("Receives error when user is locked out", () => {
+  cy.env(["lockedUser", "password"]).then(({ lockedUser, password }) => {
+    cy.visit("/");
+
+    cy.findByTestId("username").type(lockedUser);
+    cy.findByTestId("password").type(password);
+    cy.findByTestId("login-button").click();
+
+    cy.findByTestId("error")
+      .should("be.visible")
+      .and("contain.text", "Sorry, this user has been locked out.");
+    cy.url().should("not.contain", "/inventory.html");
+  });
 });
