77efe5fb09999a3e6227f2672f5567849714d399
Lonnie <llonnie21@gmail.com>
2026-02-12 07:44:03 -0700

    test(e2e): PW page objects for login/logout tests

---
+++ playwright/pageObjects/login/login.page.ts
@@ -0,0 +1,29 @@
+import type { Locator, Page } from "@playwright/test";
+
+export class LoginPage {
+  readonly page: Page;
+
+  readonly username: Locator;
+  readonly password: Locator;
+  readonly loginButton: Locator;
+  readonly error: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    this.username = page.getByTestId("username");
+    this.password = page.getByTestId("password");
+    this.loginButton = page.getByTestId("login-button");
+    this.error = page.getByTestId("error");
+  }
+
+  async goto() {
+    await this.page.goto("/");
+  }
+
+  async login(username: string, password: string) {
+    await this.username.fill(username);
+    await this.password.fill(password);
+    await this.loginButton.click();
+  }
+}
